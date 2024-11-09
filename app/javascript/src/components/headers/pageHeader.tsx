2539decd8d22ef5ca5b1698febaff2f642f3b781
marioperez094 <mario.perez094@gmail.com>
2024-11-09 10:38:29 -0700

    please delete

--- app/javascript/src/components/headers/pageHeader.tsx
+++
@@ -1,6 +0,0 @@
-//External Imports
-import React, { ReactNode } from "react";
-
-export default function PageHeader({ children }: { children: ReactNode }) {
-  return <h1 className="m-3">{ children }</h1>
-};
