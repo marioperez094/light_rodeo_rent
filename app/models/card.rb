653b2ccab5ef3c83b3a092e62fa9ab9bfca14965
marioperez094 <mario.perez094@gmail.com>
2024-12-13 01:54:05 -0700

    card fix

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,5 @@ class Card < ApplicationRecord
   belongs_to :tag, optional: true
 
   validates :image_url, presence: true
-  validates :tag, presence: true, if: -> { !isCarousel }
+  validates :tag, presence: true
 end
